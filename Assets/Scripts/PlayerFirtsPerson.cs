786b117eb9808992008d3e34ec8f0f3d9791e46f
Astrid-98 <PORTILLA159@GMAIL.COM>
2024-10-17 19:26:07 +0200

    2

--- Assets/Scripts/PlayerFirtsPerson.cs
+++ Assets/Scripts/PlayerFirtsPerson.cs
@@ -11,7 +11,7 @@ public class PlayerFirtsPerson : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-      controller =  GetComponent<CharacterController>(); // A mi variable Character controler le doy el valor igualalandola
+      controller =  GetComponent<CharacterController>(); // A mi variable CharacterController le doy el valor igualalandola
     }
 
     // Update is called once per frame
@@ -25,6 +25,19 @@ public class PlayerFirtsPerson : MonoBehaviour
         float v = Input.GetAxisRaw("Vertical"); // creo la variable float para coger el input
 
         Vector3 movimiento = new Vector3 (h, 0, v).normalized;
-        controller.Move(movimiento * velocidadMovimiento * Time.deltaTime); //con Time.deltaTime time porque esto no va por fisicas
+
+        //Atan2 es arcotangente
+        float anguloRotacion = Mathf.Atan2(movimiento.x, movimiento.z) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y; //Alineandome al angulo de la camara = Angulo al que Yo voy a rotar mi cuerpo dependiendo de a donde  mire.
+        
+        if(movimiento.magnitude > 0)
+        {
+            //Mi cuerpo queda orientado hacia donde me muevo
+            transform.eulerAngles = new Vector3(0, anguloRotacion, 0);
+
+            //me muevo hacia donde estoy orientado
+            controller.Move(movimiento * velocidadMovimiento * Time.deltaTime); //con Time.deltaTime time porque esto no va por fisicas.
+        }
+        
+
     }
 }
