e58cba428070b0035026509d22c1aa2a78111080
Astrid-98 <PORTILLA159@GMAIL.COM>
2024-10-23 18:22:24 +0200

    falta terminar

--- Assets/Scripts/ThirdPerson.cs
+++ Assets/Scripts/ThirdPerson.cs
@@ -4,12 +4,40 @@ using UnityEngine;
 
 public class ThirdPerson : MonoBehaviour
 {
+    [SerializeField] private float velocidadMovimiento;
+    [SerializeField] private float smoothTime;
+    private CharacterController controller;
+    private float velocidadRotacion;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
+    private void MoverRotar()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
 
+        Vector2 input = new Vector2 (h, v).normalized;
+
+        if (input.magnitude > 0)
+        {
+            //Calculo el angulo al que tengo que rotarme en funcion de los inputs y camara.
+            float angulo = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
+
+            //creo mi metodo anguloSuave y lo llamo desde el transform
+            float anguloSuave = Mathf.SmoothDampAngle(transform.eulerAngles.y, angulo, ref velocidadRotacion, smoothTime);
+
+            transform.eulerAngles = new Vector3(0, anguloSuave, 0);
+
+            Vector3 movimiento = Quaternion.Euler(0, angulo, 0) * Vector3.forward;
+
+            controller.Move(movimiento * velocidadMovimiento * Time.deltaTime);
+        }
+        
+
+    }
     // Update is called once per frame
     void Update()
     {
