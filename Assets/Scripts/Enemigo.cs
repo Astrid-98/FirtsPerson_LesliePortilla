b27d6bae5920fede9581aea7e0075904290fc5da
Astrid-98 <PORTILLA159@GMAIL.COM>
2024-11-04 20:22:33 +0100

    falta, esta incompleto

--- Assets/Scripts/Enemigo.cs
+++ Assets/Scripts/Enemigo.cs
@@ -9,16 +9,23 @@ public class Enemigo : MonoBehaviour
     //private Personaje player;
     //El enemigo tiene que perseguir al Player.
 
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
        //player = GameObject.FindObjectOfType<Personaje>();
+       anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //agent.SetDestination(player.gameObject.transform.position);
+        if (agent.remainingDistance <= agent.stoppingDistance) 
+        {
+        
+        }
     }
 }
